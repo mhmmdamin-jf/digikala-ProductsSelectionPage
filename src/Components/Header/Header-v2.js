733f829a66d383c6ef6fdf8c82cadf3e624942b8
mohammadamin jafari <m.a.jafari002@gmail.com>
2023-09-01 18:48:21 +0430

    style, filter, searchbox and fixed bugs.

--- src/Components/Header/Header-v2.js
+++ src/Components/Header/Header-v2.js
@@ -1,5 +1,10 @@
-export function Header() {
-      return (<>
+
+import SearchBox from "./SearchBox";
+
+export function Header({ openSearch, dispatch }) {
+
+      return (<div className="header">
+
             <div className=' d-flex'>
                   <a target='_blank' href='#'>
                         <img className='img-fluid' src="images/promo.webp" alt='a' />
@@ -12,13 +17,17 @@ export function Header() {
                                     <img src='images/لوگوی دیجیکالا.svg' className='img-fluid mt-2'></img>
                               </a>
                         </div>
-                        <div className='col-sm-6  col-md-6 col-lg-6'>
-                              <div className='px-2 px-4-lg py-2 bg-search pointer position'>
-                                    <a>
-                                          <img src='images/svgexport-3.svg' />
-                                          <span className='search-text'>جستجو</span>
-                                    </a>
-                              </div>
+                        <div className='col-sm-6 col-md-6 col-lg-6 box '>
+                              {openSearch ?
+                                    <SearchBox dispatch={dispatch} />
+                                    :
+                                    <div onClick={() => dispatch({ type: 'openSearchBox' })} className='px-2 px-4-lg py-2 bg-search pointer position'>
+                                          <a>
+                                                <img src='images/svgexport-3.svg' />
+                                                <span className='search-text'>جستجو</span>
+                                                <span className="search-shortcut">Ctrl + q</span>
+                                          </a>
+                                    </div>}
                         </div>
                         <div className='col-sm-5 col-md-4 col-lg-3 col-xl-2 me-md-auto '>
                               <div className="row align-items-center">
@@ -49,34 +58,34 @@ export function Header() {
                   <div className='row nav-font mt-2 border-bottom py-2'>
                         <div className='col-lg-9 d-md-none d-sm-none d-lg-inline '>
                               <ul className="list-group list-group-horizontal align-items-center nav-ul" >
-                                    <li className="pointer mx-2 d-sm-none d-md-none d-lg-inline">
+                                    <li className="pointer txt-underline-op mx-2 d-sm-none d-md-none d-lg-inline">
                                           <img src='images/svgexport-9.svg'></img>
                                           <span className='mx-1'>دسته بندی کالاها</span>
                                     </li>
                                     <div><span className='line mx-2 mt-2 line-1'></span></div>
-                                    <li className="pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline d-flex flex-row align-items-center">
+                                    <li className="pointer txt-underline-op mt-1 mx-2 d-sm-none d-md-none d-lg-inline d-flex flex-row align-items-center">
                                           <img className='mb-1' src='images/svgexport-23.svg'></img>
                                           <span className='mx-1 '>شگفت انگیزها </span>
                                     </li>
-                                    <li className='pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
+                                    <li className='pointer txt-underline-op mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
                                           <img className='mb-1' src='images/svgexport-24.svg'></img>
                                           <span className='mx-1'>   سوپرمارکت
                                           </span>                                   </li>
-                                    <li className='pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
+                                    <li className='pointer txt-underline-op mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
                                           <img className='mb-1' src='images/svgexport-25.svg'></img>
                                           <span className='mx-1'>  کارت هدیه
                                           </span>                                  </li>
-                                    <li className='pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
+                                    <li className='pointer mt-1 txt-underline-op mx-2 d-sm-none d-md-none d-lg-inline'>
                                           <img className='mb-1' src='images/svgexport-26.svg'></img>
                                           <span className='mx-1'>  پرفروش ترین ها
                                           </span>
                                     </li>
                                     <div><span className='line mx-2 mt-2 line-1'></span></div>
-                                    <li className='pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
+                                    <li className='pointer txt-underline-op mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
                                           <span className='mx-1'>  سوالی دارید؟
                                           </span>
                                     </li>
-                                    <li className='pointer mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
+                                    <li className='pointer txt-underline-op mt-1 mx-2 d-sm-none d-md-none d-lg-inline'>
                                           <span className='mx-1'>   در درجی کالا بفروشید!
                                           </span>
                                     </li>
@@ -103,6 +112,6 @@ export function Header() {
                         گوشی موبایل
                   </p>
             </div >
-      </>
+      </div>
       );
 }
